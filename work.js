5f306d9b6e7f27259764a7f4ede42282251d15ec
sumitt-cloud <sumit.leonova1615@gmail.com>
2026-03-10 19:57:52 +0530

    fixing photos

--- work.js
+++ work.js
@@ -1,3 +1,25 @@
+const images = document.querySelectorAll(".clicks img");
+const viewer = document.getElementById("imageViewer");
+const fullImage = document.getElementById("fullImage");
+const closeBtn = document.getElementById("closeBtn");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    viewer.style.display = "flex";
+    fullImage.src = img.src;
+  });
+});
+
+closeBtn.onclick = () => {
+  viewer.style.display = "none";
+};
+
+viewer.onclick = (e) => {
+  if(e.target !== fullImage){
+    viewer.style.display = "none";
+  }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
 
 const videos = document.querySelectorAll("video");
